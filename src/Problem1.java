0ca7b7d25599d5bfc48e95e70705c0e266118704
btemovska <btemovska@gmail.com>
2020-08-19 19:36:23 -0400

    problem 1

---
+++ src/Problem1.java
@@ -0,0 +1,34 @@
+public class Problem1 {
+    public static void main(String[] args) {
+
+        String lobster = "lobster";
+        String like = "like";
+        System.out.println(lobster.charAt(0) + "" + like.charAt(0) + "" +
+                lobster.charAt(1) + "" + like.charAt(1) + "" +
+                lobster.charAt(2) + "" + like.charAt(2) + "" +
+                lobster.charAt(3) + "" + like.charAt(3) + "" +
+                lobster.charAt(4) + "" + lobster.charAt(5) + "" +
+                lobster.charAt(6));
+        System.out.println();
+
+        String hello = "hello";
+        System.out.println(lobster.charAt(0) + "" + hello.charAt(0) + "" +
+                lobster.charAt(1) + "" + hello.charAt(1) + "" +
+                lobster.charAt(2) + "" + hello.charAt(2) + "" +
+                lobster.charAt(3) + "" + hello.charAt(3) + "" +
+                lobster.charAt(4) + "" + hello.charAt(4) + "" +
+                lobster.charAt(5) + "" + lobster.charAt(6));
+        System.out.println();
+
+        String gourmet = "gourmet";
+        System.out.println(gourmet.charAt(0) + "" + lobster.charAt(0) + "" +
+                gourmet.charAt(1) + "" + lobster.charAt(1) + "" +
+                gourmet.charAt(2) + "" + lobster.charAt(2) + "" +
+                gourmet.charAt(3) + "" + lobster.charAt(3) + "" +
+                gourmet.charAt(4) + "" + lobster.charAt(4) + "" +
+                gourmet.charAt(5) + "" + lobster.charAt(5) + "" +
+                gourmet.charAt(6) + "" + lobster.charAt(6) + "");
+
+
+    }
+}
